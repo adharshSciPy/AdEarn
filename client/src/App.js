adfdabc3056dacacb1cf64837bf41e6e0a67cf46
Sanju tl <sanjutl19@gmail.com>
2025-05-08 11:08:29 +0530

    adearn-4

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Route ,Routes} from "react-router-dom";
+import Home from "./pages/user/Login/LoginUser.jsx"
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
