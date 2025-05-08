3d386bc158fdc05d6d261aa0764cb6c712a3e20a
Vishva <adharsh.scipy@gmail.com>
2025-05-08 11:09:45 +0530

    vishva 1

--- client/pages/admin/Register/AdminSignup.jsx
+++ client/pages/admin/Register/AdminSignup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './AdminSignup.css'
 
 function AdminSignup() {
   return (
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AdminSignup from '../pages/admin/Register/AdminSignup';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/adminlogin' element={<AdminSignup />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
