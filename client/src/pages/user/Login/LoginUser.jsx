c60e6f09d20532ff3ecfa969169ad520539c0091
Sanju tl <sanjutl19@gmail.com>
2025-05-08 14:10:12 +0530

    adearn-5

--- client/src/pages/user/Login/LoginUser.jsx
+++ client/src/pages/user/Login/LoginUser.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from "react";
+import logo from "../../../assets/Logo.png";
+import styles from "./userlogin.module.css";
+import { Link } from "react-router-dom";
 
 function LoginUser() {
   return (
     <div>
-      hi
+      <div className={styles.containerOneUser}>
+        <div className={styles.containerSubUser}>
+          <div className={styles.containerTwoLeft}>
+            <div className={styles.leftMain}>
+              <div className={styles.logoContainer}>
+                <div className={styles.logo}>
+                  <img src={logo} alt="" />
+                </div>
+              </div>
+              <div className={styles.contentsContainerLeft}>
+                <div className={styles.headingMain}>
+                  <h2>Welcome Back 👋</h2>
+                </div>
+                <div className={styles.paraContent}>
+                  <p>
+                    This is a demo content.This is a demo content. This is a
+                    demo content.
+                  </p>
+                </div>
+                <div className={styles.formContainer}>
+                  <form className="form">
+                    <div>
+                      <label htmlFor="email1" className={styles.label}>
+                        Your email
+                      </label>
+                      <input
+                        id="email1"
+                        type="email"
+                        placeholder="Email@example.com"
+                        required
+                        className={styles.input}
+                      />
+                    </div>
+
+                    <div>
+                      <label
+                        htmlFor="password1"
+                        className={styles.label}
+                        style={{ marginTop: "20px" }}
+                      >
+                        Your password
+                      </label>
+                      <input
+                        id="password1"
+                        type="password"
+                        required
+                        className={styles.input}
+                        placeholder="Enter Your Password"
+                      />
+                    </div>
+                  </form>
+                </div>
+                <div className={styles.forgotPassContainer}>
+                  <Link className={styles.linkStyle}>Forgot Password?</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.containerTwoRight}>
+            <div className={styles.rightMain}></div>
+          </div>
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginUser
+export default LoginUser;
