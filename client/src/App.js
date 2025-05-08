91cb918104f75015961b95e28807dd3298519067
Vishva <adharsh.scipy@gmail.com>
2025-05-08 11:33:17 +0530

    vishva 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import AdminLogin from './pages/admin/Login/AdminLogin'
 import AdminSignup from './pages/admin/Register/AdminSignup'
+import LoginUser from './pages/user/Login/LoginUser';
+import UserSignUp from './pages/user/signUp/UserSignUp';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          {/* admin */}
+
           <Route path='/adminsignup' element={<AdminSignup />} />
           <Route path='/adminlogin' element={<AdminLogin />} />
-          
-          <Route />
+
+
+
+
+
+
+
+          {/* user */}
+          <Route path='/' element={<LoginUser/>}/>
+          <Route path='/usersignup' element={<UserSignUp/>}/>
         </Routes>
       </BrowserRouter>
 
