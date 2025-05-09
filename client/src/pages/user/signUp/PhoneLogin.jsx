5c8f4b2269fd63ee23ec6dfa933cb66cf0c08132
Sanju tl <sanjutl19@gmail.com>
2025-05-09 16:05:18 +0530

    adearn-10

---
+++ client/src/pages/user/signUp/PhoneLogin.jsx
@@ -0,0 +1,115 @@
+import { React, useState } from "react";
+import logo from "../../../assets/Logo.png";
+import styles from "./phonelogin.module.css";
+import { Link } from "react-router-dom";
+
+function PhoneLogin() {
+  const [phone, setPhone] = useState("");
+  const [error, setError] = useState("");
+
+  const validatePhone = (value) => {
+    const regex = /^\d{10}$/;
+    return regex.test(value);
+  };
+
+  const handleChange = (e) => {
+    setPhone(e.target.value);
+    if (error) setError(""); // clear error as user types
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validatePhone(phone)) {
+      setError("Enter a valid phone number starting with +91 and 10 digits.");
+      return;
+    }
+    setPhone("")
+    
+  };
+  return (
+    <div>
+      <div className={styles.containerOneUser}>
+        <div className={styles.containerSubUser}>
+          <div className={styles.containerTwoLeft}>
+            <div className={styles.leftMain}>
+              <div className={styles.logoContainer}>
+                <div className={styles.logo}>
+                  <img src={logo} alt="" />
+                </div>
+              </div>
+              <div className={styles.contentsContainerLeft}>
+                <div className={styles.contentsMainLeft}>
+                  <div className={styles.headingMain}>
+                    <h2>Welcome Back</h2>
+                  </div>
+                  <div className={styles.paraContent}>
+                    <p>This is a demo content</p>
+                  </div>
+                  <div className={styles.formContainer}>
+                    <form className="form" onSubmit={handleSubmit}>
+                      <div className={styles.formContents}>
+                        <label htmlFor="phone" className={styles.label}>
+                          Phone Number
+                        </label>
+                        <div
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "4px",
+                          }}
+                        >
+                          <span
+                            style={{
+                              padding: "15px",
+                              background: "#eee",
+                              borderRadius: "4px 4px 4px 4px",
+                            }}
+                          >
+                            +91
+                          </span>
+                          <input
+                            id="phone"
+                            type="tel"
+                            value={phone}
+                            onChange={handleChange}
+                            required
+                            className={styles.input}
+                            placeholder="10-digit number"
+                            style={{ flex: 1 }}
+                          />
+                        </div>
+                        {error && (
+                          <p style={{ color: "red", fontSize: "0.9rem",padding:"0.5rem" }}>
+                            {error}
+                          </p>
+                        )}
+                      </div>
+                      <div className={styles.buttonContainer}>
+                        <button type="submit">Submit</button>
+                      </div>
+                    </form>
+                  </div>
+                  <div className={styles.seperator}>
+                    <div className={styles.hr}></div>
+                    <p>Or</p>
+                    <div className={styles.hr}></div>
+                  </div>
+                  <div className={styles.signup}>
+                    <p>Already have an account?</p>
+                    <Link className={styles.linkStyle} to={'/'}>Login  </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.bgContainer}></div>
+          </div>
+          <div className={styles.containerTwoRight}>
+            <div className={styles.rightMain}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PhoneLogin;
