2dc234eeac60f59c9b784b704a997c36e7e3633d
gokul <gokulsunil2015@gmail.com>
2025-05-12 14:51:06 +0530

    user profile img

--- server/index.js
+++ server/index.js
@@ -4,14 +4,21 @@ import dotenv from "dotenv";
 import connectDb from "./mongoDb/connectDb.js";
 import userRouter from "./routes/userRoute.js";
 import adminRouter from "./routes/adminRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 dotenv.config();
-const app = express();
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_filename);
+const app = express();  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
 app.use(cors());
 app.use('/api/v1/user',userRouter)
+app.use("/userUploads", express.static(path.join(__dirname, "userUploads")));
 app.use('/api/v1/admin',adminRouter)
 
 const PORT = process.env.PORT || 8000;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,15 @@
 import  Router  from "express";
-import { editUser, registerUser } from "../controller/userController.js";
+import { editUser, registerUser, uploadProfilePicture, userLogin, userLogout, } from "../controller/userController.js";
+import uploadUserImg from "../multer/userImgMulter.js";
 const userRouter = Router()
 userRouter.route('/user-register').post(registerUser);
 userRouter.route('/user-update/:id').patch(editUser);
+userRouter.route('/user-login').post(userLogin);
+userRouter.route('/user-logout/:id').post(userLogout);
+userRouter.route('/user-profile-upload/:id')
+  .post(uploadUserImg, uploadProfilePicture);
+
+
+
 
 export default userRouter
