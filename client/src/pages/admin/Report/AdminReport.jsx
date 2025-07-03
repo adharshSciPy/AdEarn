344bf89612319ece26ff4c5c53acfe754b3b47a4
Vishva <adharsh.scipy@gmail.com>
2025-07-03 14:30:20 +0530

    admin report pending

--- client/src/pages/admin/Report/AdminReport.jsx
+++ client/src/pages/admin/Report/AdminReport.jsx
@@ -1,21 +1,276 @@
-import React from 'react'
-import './AdminReport.css'
+import React, { useState, useEffect } from 'react'
+import styles from './AdminReport.module.css'
 import Sidebar from '../../../components/sidebar/Sidebar'
+import Header from "../../../components/Header/Header"
+import CanvasJSReact from '@canvasjs/react-charts';
+import { Tabs, Table, Button, Avatar, Select, Pagination } from 'antd';
+import axios from 'axios'
+import baseUrl from '../../../baseurl'
+import { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
+
 
 function AdminReport() {
+
+  const id = useParams()
+  const adminId = useSelector((state) => state.admin.id)
+  const [verifyads, setVerifyads] = useState([])
+  const [verifykyc, setVerifykyc] = useState([])
+  const [assignedadminid, setAssignedadminid] = useState("")
+
+  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [selectedYear, setSelectedYear] = useState(null);
+
+  const [currentAdsPage, setCurrentAdsPage] = useState(1);
+  const [verifyAdsPage, setVerifyAdsPage] = useState(1);
+
+  const [pageSize, setPageSize] = useState(10);
+
+  const [activeTab, setActiveTab] = useState("Verified Ads");
+  const tabs = ["Verified Ads", "Verified Kyc"];
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  useEffect(() => {
+    const assignedAdmin = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/api/v1/admin`);
+        console.log("res", response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    assignedAdmin()
+  }, [])
+
+
+  useEffect(() => {
+    const adminVerifiedads = async () => {
+      try {
+        console.log("assignedid", assignedadminid)
+        const getads = await axios.get(`${baseUrl}/api/v1/admin/ads-verified/${adminId}`);
+        setVerifyads(getads.data.data)
+        console.log("getads", getads)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    adminVerifiedads()
+  }, [])
+
+  useEffect(() => {
+    const adminVerifiedkyc = async () => {
+      try {
+        console.log("admin id", adminId)
+        const getkycs = await axios.get(`${baseUrl}/api/v1/admin/kycs-verified/${adminId}`);
+        setVerifykyc(getkycs.data.data)
+        setAssignedadminid(verifykyc.kycId.assignedAdminId)
+        console.log("getkycs", getkycs)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    adminVerifiedkyc()
+
+  }, [])
+
+  // Filtered and paginated data for Verify Ads
+  const filteredVerifiedAds = useMemo(() => {
+    return verifyads.filter((ad) => {
+      const date = new Date(ad.createdAt);
+      const matchMonth = selectedMonth
+        ? (date.getMonth() + 1).toString().padStart(2, '0') === selectedMonth
+        : true;
+      const matchYear = selectedYear
+        ? date.getFullYear().toString() === selectedYear
+        : true;
+      return matchMonth && matchYear;
+    });
+  }, [verifyads, selectedMonth, selectedYear]);
+
+  const paginatedVerifiedAds = useMemo(() => {
+    const start = (currentAdsPage - 1) * pageSize;
+    const end = start + pageSize;
+    return filteredVerifiedAds.slice(start, end);
+  }, [filteredVerifiedAds, currentAdsPage, pageSize]);
+
+  // Filtered and paginated data for Verify KYC
+  const filteredVerifiedKyc = useMemo(() => {
+    return verifykyc.filter((kyc) => {
+      const date = new Date(kyc.createdAt);
+      const matchMonth = selectedMonth
+        ? (date.getMonth() + 1).toString().padStart(2, '0') === selectedMonth
+        : true;
+      const matchYear = selectedYear
+        ? date.getFullYear().toString() === selectedYear
+        : true;
+      return matchMonth && matchYear;
+    });
+  }, [verifykyc, selectedMonth, selectedYear]);
+
+  const paginatedVerifiedKyc = useMemo(() => {
+    const start = (verifyAdsPage - 1) * pageSize;
+    const end = start + pageSize;
+    return filteredVerifiedKyc.slice(start, end);
+  }, [filteredVerifiedKyc, verifyAdsPage, pageSize]);
+
+  const getTime = (isoString) => {
+    const date = new Date(isoString);
+    return date.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    });
+  };
+
+
   return (
-    <div className='adminmain'>
-    <div className='adminreportcontainermain'>
-      <Sidebar />
-      <div className="report">
-        <p>Report</p>
+    <div className={styles.adminreport}>
+      <div className={styles.adminreportcontainer}>
+        <Sidebar />
+        <Header />
+        <div className={styles.Adminreportsection}>
+          <div style={{ width: '100%', maxWidth: '1550px', height: '600px', padding: '30px' }} className={styles.AdsSection}>
+
+            <div className={styles.headsection}>
+              <h1>Reports</h1>
+              <Button>Download</Button>
+            </div>
+
+            <div className={styles.Adminads}>
+              <div
+                style={{
+                  width: "100%",
+                  maxWidth: "1550px",
+                  height: "600px",
+                  padding: "30px",
+                }}
+                className={styles.AdsSection}
+              >
+                <nav className={styles.tabMenu}>
+                  <ul>
+                    {tabs.map((tab) => (
+                      <li
+                        key={tab}
+                        className={activeTab === tab ? styles.active : ""}
+                        onClick={() => handleTabClick(tab)}
+                      >
+                        {tab}
+                      </li>
+                    ))}
+                  </ul>
+                </nav>
+
+                {activeTab === "Verified Ads" && (
+                  <section className={styles.payoutTableSection}>
+                    <h1 style={{ fontSize: "25px", padding: "10px" }}>
+                      Verify Ads
+                    </h1>
+                    <table className={styles.payoutTable}>
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Views</th>
+                          <th>Total Stars</th>
+                          <th>Start Date</th>
+                          <th>Verified Time</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {paginatedVerifiedAds.map((ad, index) => {
+                          const adRef = ad.adId.surveyAdRef || ad.adId.videoAdRef || ad.adId.imageAdRef || {};
+                          return (
+                            <tr key={index}>
+                              <td>{adRef.title || 'N/A'}</td>
+                              <td>{adRef.totalViewCount ?? 0}</td>
+                              <td>{adRef.totalStarsAllocated ?? 0}</td>
+                              <td>{new Date(adRef.createdAt).toLocaleDateString()}</td>
+                              <td>{getTime(adRef.adVerifiedTime)}</td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                    <div className={styles.pagination}>
+                      <Pagination
+                        current={currentAdsPage}
+                        pageSize={pageSize}
+                        total={filteredVerifiedAds.length}
+                        showSizeChanger
+                        pageSizeOptions={['10', '20', '50', '100']}
+                        onChange={(page, newPageSize) => {
+                          setPageSize(newPageSize);
+                          setCurrentAdsPage(page);
+                        }}
+                        style={{ marginTop: "20px", textAlign: "center" }}
+                      />
+
+                    </div>
+                  </section>
+                )}
+
+                {activeTab === "Verified Kyc" && (
+                  <section className={styles.payoutTableSection}>
+                    <h1 style={{ fontSize: "25px", padding: "10px" }}>
+                      Verify Kyc
+                    </h1>
+                    <table className={styles.payoutTable}>
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Status</th>
+                          <th>Assign Time</th>
+                          <th>ID Proof</th>
+                          <th>Move to Admin</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {paginatedVerifiedKyc.map((ad, index) => {
+                          const adRef = ad.kycId || {};
+                          return (
+                            <tr key={index}>
+                              <td>{adRef.fullName || 'N/A'}</td>
+                              <td>{adRef.kycStatus ?? 0}</td>
+                              <td>{getTime(adRef.assignmentTime)}</td>
+                              <td>{adRef.documentType}</td>
+                              <td>{getTime(adRef.adVerifiedTime)}</td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                    <div className={styles.pagination}>
+                      <Pagination
+                        current={verifyAdsPage}
+                        pageSize={pageSize}
+                        total={filteredVerifiedKyc.length}
+                        showSizeChanger
+                        pageSizeOptions={['10', '20', '50', '100']}
+                        onChange={(page, newPageSize) => {
+                          setPageSize(newPageSize);
+                          setVerifyAdsPage(page);
+                        }}
+                        style={{ marginTop: "20px", textAlign: "center" }}
+                      />
+
+                    </div>
+                  </section>
+                )}
+              </div>
+            </div>
+
+
+          </div>
+        </div>
       </div>
-
-
-
-
     </div>
-  </div>
   )
 }
 
