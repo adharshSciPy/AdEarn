a0a102b445d46c53de4d16f30894b8a3760ada0a
Sanju tl <sanjutl19@gmail.com>
2025-07-04 16:04:40 +0530

    this commit

--- client/src/pages/admin/AdminCouponReq/AdminCouponReq.jsx
+++ client/src/pages/admin/AdminCouponReq/AdminCouponReq.jsx
@@ -5,16 +5,21 @@ import baseUrl from "../../../baseurl";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 function AdminCouponReq() {
-    const [bundles, setBundles] = useState([]);
-  const getCoupons=async()=>{
+  const [bundles, setBundles] = useState([]);
+  const getCoupons = async () => {
     try {
-      const response=await axios.get(`${baseUrl}/api/v1/`)
-      
+      const response = await axios.get(
+        `${baseUrl}/api/v1/admin/coupons-requests`
+      );
+      console.log(response);
+      setBundles(response.data.data || []);
     } catch (error) {
       console.log(error);
-      
     }
-  }
+  };
+  useEffect(() => {
+    getCoupons();
+  }, []);
   return (
     <div className={styles.UserAccount}>
       <Sidebar />
@@ -24,26 +29,34 @@ function AdminCouponReq() {
         </div>
 
         <div className={styles.grid}>
-          {bundles.map((bundle) => (
-            <div
-              key={bundle._id}
-              className={styles.card}
-            >
-              <div className={styles.cardContent}>
-                <h4>Bundle ID: {bundle.batchId}</h4>
-                <div className={styles.viewDetails}>
-                  <span>Click to view details</span>
-                  <span className={styles.arrowIcon}>→</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          <table className={styles.bundleTable}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>UserName</th>
+                <th>Note</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bundles.map((bundle, index) => (
+                <tr key={bundle._id}>
+                  <td>{index + 1}</td>
+                  <td>{bundle.userName}</td>
+                  <td>
+                    {bundle.note}
+                  </td>
+                  <td>
+                    <button className={styles.sendButton}>Send</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-
-        
       </div>
     </div>
-  )
+  );
 }
 
-export default AdminCouponReq
+export default AdminCouponReq;
