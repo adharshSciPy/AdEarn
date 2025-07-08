fa22b0100b8e1861fd26edad50a9e845a1808a84
gokul <gokulsunil2015@gmail.com>
2025-07-08 10:03:10 +0530

    admin coupon distribution and get api

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -18,7 +18,8 @@ import {
     updateAdmin,
     getAssignedCoupons,
     approveCouponRequest,
-    assignCouponsToUserRequest
+    assignAndApproveCouponRequest,
+    getCouponRequestDetails
 } from "../controller/adminController.js";
 
 const adminRouter = Router()
@@ -55,12 +56,12 @@ adminRouter.route('/kycs-rejected/:id').get(getKycsRejectedByAdmin);//fetch reje
 adminRouter.route("/adminget/:id").get(getAdminById);
 adminRouter.route("/coupons-requests").get(fetchAllCouponRequest);//to fetch all coupon requests from user
 adminRouter.route("/coupon/single-request").get(fetchSingleCouponRequest);//to fetch single coupon request from user
-adminRouter.route("/assigned-coupon-requests/:id").get(fetchCouponRequestsAssignedToAdmin);//to fetch single coupon request from user
+adminRouter.route("/assigned-coupon-requests/:id").get(fetchCouponRequestsAssignedToAdmin);//to fetch  coupon request for assigned admin
 adminRouter.route('/assign-coupon-admin/:id').post(assignBatchToAdmin);//admin can mannually select coupons  to verify ;
 adminRouter.route('/approve-coupon/:id').post(approveCouponRequest)
 adminRouter.route('/assigned-coupons/:adminId').get(getAssignedCoupons);//to fetch coupons assigned by superadmin NOT USE IT ALREADY INTEGRATED
-adminRouter.route('/distribute-coupon/:id').post(assignCouponsToUserRequest)
-
+adminRouter.route('/distribute-coupon/:id').post(assignAndApproveCouponRequest)// this is the route to approve and distribute coupons to user for logging purpose
+adminRouter.route('/coupon/approved/request-details/:id').get(getCouponRequestDetails)
 
 
 
