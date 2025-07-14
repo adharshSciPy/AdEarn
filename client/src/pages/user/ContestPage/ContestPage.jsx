892cb826a1ea44cbe0e41eca54ae1a5d0ebee7cf
Sanju tl <sanjutl19@gmail.com>
2025-07-14 10:35:46 +0530

    npm i

--- client/src/pages/user/ContestPage/ContestPage.jsx
+++ client/src/pages/user/ContestPage/ContestPage.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import styles from "./ContestPage.module.css";
 import { Link } from "react-router-dom";
 import Navbar from "../NavBar/Navbar";
-import  { useEffect, useState } from 'react'
-import axios from 'axios';
-import baseUrl from '../../../baseurl'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import baseUrl from "../../../baseurl";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const contests = [
   {
@@ -77,34 +80,42 @@ const ContestPage = () => {
         </div>
 
         <div className={styles.cardContainer}>
-          {contest?.map((contest) => (
-            <div className={styles.card} key={contest.id}>
-              <div className={styles.cardHeader}>{contest.title}</div>
-              <div className={styles.cardContent}>
-                <div className={styles.lockIcon}>
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M4 6C4 3.79086 5.79086 2 8 2H14V6C14 8.20914 15.7909 10 18 10H20V18C20 20.2091 18.2091 22 16 22H8C5.79086 22 4 20.2091 4 18V6ZM8 11C7.44772 11 7 11.4477 7 12C7 12.5523 7.44772 13 8 13H10C10.5523 13 11 12.5523 11 12C11 11.4477 10.5523 11 10 11H8ZM8 15C7.44772 15 7 15.4477 7 16C7 16.5523 7.44772 17 8 17H12C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15H8ZM16.6818 4.19879L16.5509 6.16288C16.5106 6.76656 17.0115 7.26743 17.6152 7.22718L19.5792 7.09624C20.4365 7.03909 20.8274 5.99887 20.2198 5.39135L18.3867 3.5582C17.7792 2.95068 16.7389 3.34153 16.6818 4.19879Z"
-                      fill="orange"
+          {contest?.map((item, index) => (
+            <div className={styles.card} key={index}>
+              <Swiper
+                spaceBetween={10}
+                slidesPerView={1}
+                className={styles.carousel}
+              >
+                {item.prizeImages?.map((img, i) => (
+                  <SwiperSlide key={i}>
+                    <img
+                      src={`${baseUrl}${img}`}
+                      alt={`Prize ${i + 1}`}
+                      className={styles.prizeImage}
                     />
-                  </svg>
-                </div>
-                <div className={styles.date}>{contest.createdAt}</div>
-                <div className={styles.entry}>{contest.entry}</div>
-                <div className={styles.entryDetails}>
-                  {contest.entryStars}
-                </div>
-                <div className={styles.details}>{contest.winnerSelectionType}</div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
 
-                <div className={styles.buttonContainer}>
+              <div className={styles.cardContent}>
+                <h2 className={styles.title}>{item.contestName}</h2>
+                <p>
+                  <strong>Max Entries:</strong> {item.maxParticipants}
+                </p>
+                <p>
+                  <strong>Entry Stars:</strong> {item.entryStars} ⭐ 
+                </p>
+                {item.startDate && (
+                  <p>
+                    <strong>Start Date:</strong>{" "}
+                    {new Date(item.startDate).toLocaleDateString()}
+                  </p>
+                )}
+                <p>
+                  <strong>Status:</strong> {item.status}
+                </p>
+                <div className={styles.buttonWrapper}>
                   <button className={styles.enterButton}>Enter</button>
                 </div>
               </div>
